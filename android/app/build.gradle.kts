11f236c4ae16ba12d3f3267fb6a0116dbb142b0e
bvlion <bvltiggeari@ambitious-i.net>
2026-09-18 08:20:40 +0900

    Set Android application identifier

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-    id("com.android.application")
+  id("com.android.application")
 }
 
 android {
-    namespace = "com.bvlion.mealrelay"
-    compileSdk = 37
+  namespace = "net.ambitious.android.mealrelay"
+  compileSdk = 37
 
-    defaultConfig {
-        applicationId = "com.bvlion.mealrelay"
-        minSdk = 37
-        targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
-    }
+  defaultConfig {
+    applicationId = "net.ambitious.android.mealrelay"
+    minSdk = 37
+    targetSdk = 37
+    versionCode = 1
+    versionName = "1.0"
+  }
 }
